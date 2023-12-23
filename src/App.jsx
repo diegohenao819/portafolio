8b3a49503858373a1a10c453f8a46a25e8f53f90
diegohenao819 <diegohenao819@gmail.com>
2023-12-23 16:36:13 -0500

    app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./app.css";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import NavBar from "./components/navBar/NavBar";
